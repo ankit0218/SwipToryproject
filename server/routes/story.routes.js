fbbd05a40d2b03c5b57c5afc2ff4139d642a0374
Harit Sharma <haritsharma058@gmail.com>
2023-09-04 07:32:54 +0530

    uploading

--- server/routes/story.routes.js
+++
@@ -1,7 +0,0 @@
-const {Router}=require("express");
-const routes=Router();
-const stories=require("../controller/stories.controller");
-const details = require("../controller/details.controller");
-routes.get("/",stories)
-routes.get("/details",details)
-module.exports=routes;
