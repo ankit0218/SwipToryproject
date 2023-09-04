aeb484b768138a3ae2a1056a4ab5866d709358f2
Harit Sharma <haritsharma058@gmail.com>
2023-09-04 08:00:29 +0530

    mongoose.config update

--- server/config/mongo.config.js
+++ server/config/mongo.config.js
@@ -1,9 +1,5 @@
 const mongoose=require("mongoose");
-module.exports=mongoose.connect("mongodb://ankit_db/swiptory").then(()=>{
+module.exports=mongoose.connect("mongodb://127.0.0.1:27017/swiptory").then(()=>{
     console.log("db connected");
 });
-
-// module.exports=mongoose.connect("mongodb://127.0.0.1:27017/swiptory").then(()=>{
-//     console.log("db connected");
-// });
 
